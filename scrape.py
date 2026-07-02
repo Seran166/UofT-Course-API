d2e254fd99f1a40c6118e813161e23b3c68f0015
Seran166 <seran.naventhan@gmail.com>
2026-07-02 05:29:05 -0400

    Scraping, and using the internal uoft api to get data.

---
+++ scrape.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup, Tag
+import pprint
+
+def parse_html_with_link(response: requests.Response):
+    """
+    Preconditions:
+        response.status_code == 200
+    """
+
+    html_content = response.content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    parse_html(soup)
+
+
+def parse_html_with_file():
+    with open('uoft.html', 'r') as f:
+        html_content = f.read()
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+    parse_html(soup)
+
+
+def find_field_content(page: Tag, class_field: str) -> Tag | None:
+    span = page.find('span', class_=class_field)
+    if span is not None:
+        field_content = span.find('span', class_='field-content')
+        if field_content is not None:
+            return field_content
+
+    return None
+
+
+def find_field_content_body(page: Tag, class_field: str) -> Tag | None:
+    span = page.find('div', class_=class_field)
+    if span is not None:
+        field_content = span.find('span', class_='field-content')
+        if field_content is not None:
+            return field_content
+
+    return None
+    
+
+def parse_html(soup: BeautifulSoup):
+    course_pages = soup.find_all('div', class_='no-break w3-row views-row')
+    courses = {}
+    for page in course_pages:
+        title = page.h3.text if page.h3 is not None else ""
+        course_code = title[:8]
+
+        course_hours = find_field_content(page, 'views-field views-field-field-hours')
+        course_hours = course_hours.text.removesuffix('\n') if course_hours is not None else None
+        
+        description = find_field_content_body(page, 'views-field views-field-body')
+        description = description.text if description is not None else None
+
+        prerequisites = find_field_content(page, 'views-field views-field-field-prerequisite')
+        prerequisites = prerequisites.text if prerequisites is not None else None
+        
+        exclusion = find_field_content(page, 'views-label views-label-field-exclusion')
+        exclusion = exclusion.text if exclusion is not None else None
+
+        recommended = find_field_content(page, 'views-field views-field-field-recommended')
+        recommended = recommended.text if recommended is not None else None
+
+        breadth = find_field_content(page, 'views-field views-field-field-breadth-requirements')
+        breadth = breadth.text if breadth is not None else None
+
+        course_info = {
+                "course code": course_code,
+                "course hours": course_hours, 
+                "description": description, 
+                "prerequisites": prerequisites, 
+                "exclusion": exclusion,
+                "recommended": recommended, 
+                "breadth": breadth
+            }
+        
+        courses[course_code] = course_info
+
+    pprint.pprint(courses)
+    print(len(courses))
+
+
+print("Running...")
+url = 'https://artsci.calendar.utoronto.ca/print/view/pdf/course_search/print_page/debug?page=0'
+
+response = requests.get(url)
+if response.status_code == 200:
+    parse_html_with_link(response)
+else:
+    print(response.status_code)
---
+++ uoft_api.py
@@ -0,0 +1,111 @@
+from typing import Any
+
+import requests
+from requests import Response
+import json
+import pprint
+from dataclasses import dataclass
+
+url = "https://api.easi.utoronto.ca/ttb/getPageableCourses"
+# @dataclass
+class Course:
+    c = 'a'
+    
+    """
+    Preconditions:
+     - self.specific_id.isalnum()
+     - self.section == 'F' or self.section == 'S'
+     - self.code
+    """
+    def __init__(self, course_response: Response) -> None:
+        course_data = dict(course_response.json())['payload']['pageableCourse']['courses'][0]
+        course_info = course_data['cmCourseInfo']
+
+        self.specific_id = course_data['id']
+        self.name = course_data['name']
+        self.section = course_data['sectionCode']
+        self.code = course_data['code']
+
+        self.title = course_info['title']
+        if self.title != self.name:
+            print(f'{self.title} vs {self.name}')
+
+        self.description = course_info['description']
+        self.prerequisites = course_info['prerequisitesText']
+        self.corequisites = course_info['corequisitesText']
+        self.exclusions = course_info['exclusionsText']
+        self.recommended = course_info['recommendedPreparation']
+
+        self.breadth_s = course_info['breadthRequirements']
+
+        self.department = course_data['department']['name']
+        self.department_code = course_data['department']['code']
+        
+        sections = course_data['sections']
+        for lec_or_tut in sections:
+            print(f'{lec_or_tut['type']}: {lec_or_tut['name']}')
+
+
+# data_raw = {
+#     "courseCodeAndTitleProps": {
+#         "courseCode":"AFR460H1",
+#         "courseTitle":"Climate Change, Food Security, and Sustainability in Africa",
+#         "courseSectionCode":"F",
+#         "searchCourseDescription":False},
+#     "departmentProps":[],
+#     "campuses":[],
+#     "sessions":["20269","20271","20269-20271"],
+#     "requirementProps":[],
+#     "instructor":"",
+#     "courseLevels":[],
+#     "deliveryModes":[],
+#     "dayPreferences":[],
+#     "timePreferences":[],
+#     "divisions":["ARTSC"],
+#     "creditWeights":[],
+#     "availableSpace":False,
+#     "waitListable":False,
+#     "page":1,
+#     "pageSize":20,
+#     "direction":"asc"}
+
+payload = {
+    "courseCodeAndTitleProps": {
+        "courseCode":"CSC111H1",
+        "courseTitle":"Foundations of Computer Science II",
+        "courseSectionCode":"S",
+        "searchCourseDescription": False},
+    "departmentProps":[],
+    "campuses":[],
+    "sessions":["20269","20271","20269-20271"],
+    "requirementProps": [],
+    "instructor": "",
+    "courseLevels":[],
+    "deliveryModes":[],
+    "dayPreferences":[],
+    "timePreferences":[],
+    "divisions":["ARTSC"],
+    "creditWeights":[],
+    "availableSpace":False,
+    "waitListable":False,
+    "page":1,""
+    "pageSize":20,"direction":"asc"}
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Origin": "https://ttb.utoronto.ca",
+    "Referer": "https://ttb.utoronto.ca/"
+}
+
+response = requests.post(
+    url,
+    headers=headers,
+    json=payload,
+    timeout=30
+)
+
+Course(response)
+
+
+
